454e69c73ecea356595fbf0454ddf308a279a99f
Edward Akapo <edwardakapo@outlook.com>
2024-07-02 23:22:34 -0400

    removed cookie consent until fixes can be implemented later on

---
+++ callous-crater/src/components/CookieConsentConfig.ts
@@ -0,0 +1,69 @@
+// import type { CookieConsentConfig } from 'vanilla-cookieconsent';
+
+// export const config: CookieConsentConfig = {
+//     guiOptions: {
+//         consentModal: {
+//             layout: "box",
+//             position: "bottom left",
+//             equalWeightButtons: true,
+//             flipButtons: false
+//         },
+//         preferencesModal: {
+//             layout: "box",
+//             position: "right",
+//             equalWeightButtons: true,
+//             flipButtons: false
+//         }
+//     },
+//     categories: {
+//         necessary: {
+//             readOnly: true
+//         },
+//         analytics: {},
+//         marketing: {}
+//     },
+//     language: {
+//         default: "en",
+//         autoDetect: "browser",
+//         translations: {
+//             en: {
+//                 consentModal: {
+//                     title: "Cookie Settings",
+//                     description: "Here you can customize your cookie preferences. Please note that blocking some types of cookies may impact your experience on our site and the services we can offer",
+//                     acceptAllBtn: "Accept all",
+//                     acceptNecessaryBtn: "Reject all",
+//                     showPreferencesBtn: "Manage preferences",
+//                     footer: "<a href=\"#link\">Privacy Policy</a>\n<a href=\"#link\">Terms and conditions</a>"
+//                 },
+//                 preferencesModal: {
+//                     title: "Consent Preferences Center",
+//                     acceptAllBtn: "Accept all",
+//                     acceptNecessaryBtn: "Reject all",
+//                     savePreferencesBtn: "Save preferences",
+//                     closeIconLabel: "Close modal",
+//                     serviceCounterLabel: "Service|Services",
+//                     sections: [
+//                         {
+//                             title: "Cookie Usage",
+//                             description: "Our website uses cookies to improve your experience, understand how our website is being used, and serve you personalized content. Cookies are small text files stored on your device that help us provide and enhance our services. Below is an overview of the types of cookies we use and their purposes"
+//                         },
+//                         {
+//                             title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
+//                             description: "These cookies are essential for the website to function and cannot be switched off",
+//                             linkedCategory: "necessary"
+//                         },
+//                         {
+//                             title: "Analytics Cookies",
+//                             description: "These cookies collect information about how visitors use our website, such as which pages are visited most often and any error messages received. The information collected is aggregated and anonymous, and it helps us improve the way our website works.",
+//                             linkedCategory: "analytics"
+//                         },
+//                         {
+//                             title: "More information",
+//                             description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+//                         }
+//                     ]
+//                 }
+//             }
+//         }
+//     }
+// };
